b3d41a3ceaebef13562b490e31733b38e443e0eb
gcmello <gcmello@live.com>
2021-04-26 17:21:34 -0300

    FirstCommit

---
+++ website/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Post(models.Model):#cria uma classe Post dentro de models.Model
+    title = models.CharField(max_length=100)#dis que sub_title é um campo de palavras definido em 100 caracteres
+    sub_title = models.CharField(max_length=200)#dis que sub_title é um campo de palavras definido em 200 caracteres
+    content = models.TextField()#dis que o content é um campo de texto;
+
