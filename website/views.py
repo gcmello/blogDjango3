89c36d8983eba271cc212c2b389e79a6e05f2f99
gcmello <gcmello@live.com>
2021-04-26 20:58:32 -0300

    Aula de templates

--- website/views.py
+++ website/views.py
@@ -1,3 +1,12 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello_blog(request):
+
+    lista = ['Django', 'Python', 'Git', 'HTML', 'BDados', 'Linux', 'Nginx', 'Uwsgi', 'Systemctl']
+
+    data = {'name': 'Curso de Django', 'lista_tecno' : lista}
+    
+    return render(request, 'index.html', data)
+
