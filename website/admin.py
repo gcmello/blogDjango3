4553a86e89fd429f92f1e88b1b28a1cf49735570
gcmello <gcmello@live.com>
2021-04-27 11:42:22 -0300

    Modificações no Django Admin

--- website/admin.py
+++ website/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.forms.forms import DeclarativeFieldsMetaclass
 from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'sub_title', 'full_name']
+    list_display = ['title', 'sub_title', 'full_name', 'categorias','deleted']
     search_fields = ['title', 'sub_title']
+
+    def get_queryset(self, request):
+        return Post.objects.filter(deleted = True)
+        
+
 
 
 
--- website/models.py
+++ website/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
+class Categorias(models.TextChoices):
+    TECH = 'TC' , 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+    
+
+
 class Post(models.Model):#cria uma classe Post dentro de models.Model
     title = models.CharField(max_length=100)#dis que sub_title é um campo de palavras definido em 100 caracteres
     sub_title = models.CharField(max_length=200)#dis que sub_title é um campo de palavras definido em 200 caracteres
     content = models.TextField()#dis que o content é um campo de texto;
+
+    categorias = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR,
+    )
+
+    deleted = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
